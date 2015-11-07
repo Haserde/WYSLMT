1db7f67fbe5cfc66765fb4af12aaec0678189fb9
Hasibe Erdemir <erdeh815@newschool.edu>
2015-11-07 14:18:56 -0500

    textbox manager update

--- WYSLMT FINAL/Assets/Scenes/Scripts/TextBoxManager.cs
+++ WYSLMT FINAL/Assets/Scenes/Scripts/TextBoxManager.cs
@@ -14,6 +14,14 @@ public class TextBoxManager : MonoBehaviour {
 	public int currentLine;
 	public int endAtLine;
 
+	private bool isTyping = false;
+	private bool cancelTyping = false;
+
+
+	public float typeSpeed;
+
+
+
 	//public PlayerController player;
 	
 	void Start () {
@@ -27,12 +35,42 @@ public class TextBoxManager : MonoBehaviour {
 	}
 
 	void Update() {
-	theText.text = textLines[currentLine];
+	
+		//theText.text = textLines[currentLine];
 
 		if (Input.GetKeyDown (KeyCode.Return)) {
-			currentLine += 1;
+			if (!isTyping) {
+				currentLine += 1;
+
+				if (currentLine > endAtLine) {
+					DisableTextBox ();
+				} else {
+					StartCoroutine(TextScroll(textLines[currentLine]));
+				}
+			} else if (isTyping && !cancelTyping) {
+				cancelTyping = true;
+			}
 		}
-	
+	}
+
+	private IEnumerator TextScroll (string lineofText){
+		int letter = 0;
+		theText.text = "";
+		isTyping = true;
+		cancelTyping = false;
+		while (isTyping && !cancelTyping && (letter < lineofText.Length - 1)) {
+			theText.text += lineofText [letter];
+			letter += 1;
+			yield return new WaitForSeconds(typeSpeed);
+		}
+		theText.text = lineofText;
+		isTyping = false;
+		cancelTyping = false;
+	}
+
+
+	public void DisableTextBox(){
+		textBox.SetActive (false);
 	}
 
 }
