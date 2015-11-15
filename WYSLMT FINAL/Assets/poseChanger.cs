3c182a166e4220d0682989c77a24daccf2898731
Guilherme Coelho <guilhermeismco@gmail.com>
2015-11-15 18:21:17 -0500

    updated the dancing game more graphic stuff and visual cues

---
+++ WYSLMT FINAL/Assets/poseChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class poseChanger : MonoBehaviour {
+	
+	Animator changePose;
+	public Slider slider;
+	public GameObject TextWarning;
+	public GameObject EndDance;
+
+
+	void Start () {
+
+		changePose = GetComponent<Animator> ();
+	
+	}
+
+	void Update () {
+
+		if (slider.value >= 20 && slider.value < 75) {
+
+			changePose.SetInteger ("State", 1);
+			Instantiate (TextWarning, GameObject.Find ("textPos").transform.position, GameObject.Find ("textPos").transform.rotation);
+		
+
+		} else if (slider.value >= 75) {
+			changePose.SetInteger ("State", 0);
+
+		} else {
+			changePose.SetInteger("State", 3);
+		}
+	
+	}
+
+}
