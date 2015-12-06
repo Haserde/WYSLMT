f410b53ddd19dc46cedadbe49b3e23d89bfd9c6a
Guilherme Coelho <guilhermeismco@gmail.com>
2015-12-06 00:14:34 -0500

    some progress in the talking game, laugh button checks state

---
+++ WYSLMT FINAL/Assets/changeExpressions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeExpressions : MonoBehaviour {
+
+	Animator datePoses;
+
+	// Use this for initialization
+	void Start () {
+
+		datePoses = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.A)) {
+
+			datePoses.SetInteger("State", 2);
+
+		}
+
+	
+
+	
+	}
+}
---
+++ WYSLMT FINAL/Assets/checkLaugh.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkLaugh : MonoBehaviour {
+	
+
+	public void checkL() {
+
+		if (GameObject.Find ("Date").GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Wink")) {
+
+			Debug.Log ("score+1");
+
+		} else {
+
+			Debug.Log ("Score-1");
+		}
+
+	}
+}
