c6ef5cc6456dc24c8321b4fe98596be403049473
Guilherme Coelho <guilhermeismco@gmail.com>
2015-12-14 16:00:50 -0500

    no message

--- WYSLMT FINAL/Assets/Scripts/newPoses.cs
+++ WYSLMT FINAL/Assets/Scripts/newPoses.cs
@@ -23,7 +23,7 @@ public class newPoses : MonoBehaviour {
 
 		if (scoreSlider.value > 0 && scoreSlider.value < 80) {
 
-			Invoke ("scoreDown", 3);
+			Invoke ("scoreDown", 5);
 
 		} else if (scoreSlider.value > 80 && scoreSlider.value < 100) {
 
