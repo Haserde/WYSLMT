065310271b9bef65fe81aa50cad427607dd2fc32
Hasibe Erdemir <erdeh815@newschool.edu>
2015-12-16 16:19:39 -0500

    camera position script

--- WYSLMT FINAL/Assets/moveCamera.cs
+++ WYSLMT FINAL/Assets/moveCamera.cs
@@ -4,17 +4,41 @@ using System.Collections;
 public class moveCamera : MonoBehaviour {
 
 	public int xMove = 1;
+	public int Boundary = 50;
+	public int speed = 5;
+
+	private int theScreenWidth;
+	private int theScreenHeight;
 
 	// Use this for initialization
 	void Start () {
-	
+		theScreenWidth = Screen.width;
+		theScreenHeight = Screen.height;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		if (Input.mousePosition.x > theScreenWidth - Boundary) {
+			transform.position.x += speed * Time.deltaTime;
+		}
+		if (Input.mousePosition.x < 0 + Boundary) {
+			transform.position.x -= speed * Time.deltaTime;
+		}
+		if (Input.mousePosition.y > theScreenHeight - Boundary) {
+			transform.position.y += speed * Time.deltaTime;
+		}
+		if (Input.mousePosition.y < 0 + Boundary) {
+			transform.position.y -= speed * Time.deltaTime;
+		}
+	}
 
-		if(Input.GetKeyDown(KeyCode.A)){
+
+
+
+
+
+		/*if(Input.GetKeyDown(KeyCode.A)){
 		transform.position = transform.position + new Vector3 (-xMove, 0, 0);
 
 		}
@@ -22,7 +46,5 @@ public class moveCamera : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.D)){
 		transform.position = transform.position + new Vector3 (xMove, 0, 0);
 		
-		}
-
-	}
+		}*/
 }
