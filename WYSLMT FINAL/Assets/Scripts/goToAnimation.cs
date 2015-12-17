7351f0c7cb501a91ae255ff4f789795120631e10
Guilherme Coelho <guilhermeismco@gmail.com>
2015-12-17 03:24:09 -0500

    linked middle scenes

--- WYSLMT FINAL/Assets/Scripts/goToAnimation.cs
+++ WYSLMT FINAL/Assets/Scripts/goToAnimation.cs
@@ -4,10 +4,27 @@ using UnityEngine.UI;
 
 public class goToAnimation : MonoBehaviour {
 
+	public GameObject date;
+
 
 	public void finalMove(){
 
-		Application.LoadLevel("Final Scene");
+		if (date.tag == "Hasibe") {
+
+			Application.LoadLevel ("Middle Scene");
+		}
+
+		if (date.tag == "Gui") {
+
+			Application.LoadLevel("Gui Middle Scene");
+
+		}
+
+		if (date.tag == "Nicki") {
+
+			Application.LoadLevel("Nicki Middle Scene");
+
+		}
 
 	}
 }
