8448696b5202612fe3756a248119ea2738b94014
Guilherme Coelho <guilhermeismco@gmail.com>
2015-12-17 02:00:46 -0500

    no message

---
+++ WYSLMT FINAL/Assets/moveClothes.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveClothes : MonoBehaviour {
+
+	public float forceX = 0;
+	public float forceY;
+	public int orientation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		forceX += .1f;
+		forceY += .2f;
+
+		gameObject.GetComponent<Rigidbody> ().AddForce (.1f * orientation, forceY, 0);
+
+//		if (forceX >= 2) {
+//
+//			forceX = 0;
+//		} else if (forceY >= 3) {
+//
+//			forceY = 0;
+//		}
+
+		Destroy (gameObject, 4);
+	
+	}
+}
---
+++ WYSLMT FINAL/Assets/spawnClothes.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnClothes : MonoBehaviour {
+
+
+	public GameObject clothesToSpawn;
+	public GameObject place;
+	public int timer;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Invoke ("clothesS", timer);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void clothesS(){
+
+		Instantiate (clothesToSpawn, place.gameObject.transform.position, Quaternion.identity);
+
+	}
+}
