53fd013499aa9c2e0eadc4928dd0bb90c7f61203
Hasibe Erdemir <erdeh815@newschool.edu>
2015-12-17 04:27:36 -0500

    start screen music and sound effects :)

---
+++ WYSLMT FINAL/Assets/bouncerTalk.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouncerTalk : MonoBehaviour {
+
+	public AudioSource bouncer;
+
+	// Use this for initialization
+	void Start () {
+		bouncer = bouncer;
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseEnter() {
+		bouncer.Play ();
+	
+	}
+}
